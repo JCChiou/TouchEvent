f4dfc905a24a5a8e008d9b1bc18059eda24b0497
JCChiou <rex2763@gmail.com>
2021-03-15 22:45:17 +0800

    use GestureDetectorCompat

--- app/src/main/java/com/example/mygesture/MainActivity.kt
+++ app/src/main/java/com/example/mygesture/MainActivity.kt
@@ -2,25 +2,22 @@ package com.example.mygesture
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.GestureDetector
-import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
+import android.view.View.OnGenericMotionListener
+import android.view.View.OnTouchListener
 import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MotionEventCompat
+import androidx.core.view.GestureDetectorCompat
 import timber.log.Timber
-import java.util.jar.Attributes
 
 
 class MainActivity : AppCompatActivity() {
 
     private var mgListener: MyGestureListener? = null
-    private var mDetector: GestureDetector? = null
+    private lateinit var mDetector: GestureDetectorCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,18 +26,18 @@ class MainActivity : AppCompatActivity() {
             Timber.plant(Timber.DebugTree())
         }
         mgListener = MyGestureListener("這是Activity發送")
-        mDetector = GestureDetector(this,mgListener)
+        mDetector = GestureDetectorCompat(this, mgListener)
 
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-//        Timber.d("demo,MainActivity-----------onTouchEvent------  ${event.toString()}")
-        mDetector!!.onTouchEvent(event)
+        Timber.d("demo,MainActivity-----------onTouchEvent------  ${event.toString()}")
+        mDetector.onTouchEvent(event)
         return super.onTouchEvent(event)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-//        Timber.d("demo,MainActivity-----------dispatchTouchEvent--${ev.toString()}")
+        Timber.d("demo,MainActivity-----------dispatchTouchEvent--${ev.toString()}")
         return super.dispatchTouchEvent(ev)
     }
 }
@@ -48,13 +45,13 @@ class MainActivity : AppCompatActivity() {
 class SimpleGestureListener(info: String) : GestureDetector.SimpleOnGestureListener() {
     val context = info
     override fun onDown(e: MotionEvent?): Boolean {
-        return true
-//        return super.onDown(e)
+//        return true
+        return super.onDown(e)
     }
 
     override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        return true
-//        return super.onSingleTapUp(e)
+//        return true
+        return super.onSingleTapUp(e)
     }
 
 }
@@ -64,16 +61,16 @@ class MyGestureListener(info: String) : GestureDetector.OnGestureListener {
     override fun onDown(e: MotionEvent?): Boolean {
         Timber.d("onDown ,${context}")
 
-        return false
-//        return true
+//        return false
+        return true
     }
 
     override fun onShowPress(e: MotionEvent?) {
-        Timber.d("onShowPress, $context")
+//        Timber.d("onShowPress, $context")
     }
 
     override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        Timber.d("onSingleTapUp, ${context}")
+//        Timber.d("onSingleTapUp, ${context}")
         return false
     }
 
@@ -83,7 +80,7 @@ class MyGestureListener(info: String) : GestureDetector.OnGestureListener {
     }
 
     override fun onLongPress(e: MotionEvent?) {
-        Timber.d("onLongPress, ${context}")
+//        Timber.d("onLongPress, ${context}")
     }
 
     override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
@@ -95,13 +92,14 @@ class MyGestureListener(info: String) : GestureDetector.OnGestureListener {
 
 class Mygroupview(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     private var linemgListener: MyGestureListener? = null
-    private var linemDetector: GestureDetector? = null
+    private var linemDetector: GestureDetectorCompat
     init {
         linemgListener = MyGestureListener("這是LinearLayout發送")
-        linemDetector = GestureDetector(context,linemgListener)
+        linemDetector = GestureDetectorCompat(context, linemgListener)
     }
+
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
-//        Timber.d("demo,LinearLayout--------onInterceptTouchEvent---${ev.toString()}")
+        Timber.d("demo,LinearLayout--------onInterceptTouchEvent---${ev.toString()}")
 //        linemDetector!!.onTouchEvent(ev)
 //        return true
         return super.onInterceptTouchEvent(ev)
@@ -109,14 +107,14 @@ class Mygroupview(context: Context, attrs: AttributeSet) : LinearLayout(context,
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-//        Timber.d("demo,LinearLayout---------onTouchEvent-----------${event.toString()}")
-        linemDetector!!.onTouchEvent(event)
+        Timber.d("demo,LinearLayout---------onTouchEvent-----------${event.toString()}")
+        linemDetector.onTouchEvent(event)
         return true
 //        return super.onTouchEvent(event)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-//        Timber.d("demo,LinearLayout------------dispatchTouchEvent--${ev.toString()}")
+        Timber.d("demo,LinearLayout------------dispatchTouchEvent--${ev.toString()}")
         return super.dispatchTouchEvent(ev)
     }
 }
@@ -124,21 +122,23 @@ class Mygroupview(context: Context, attrs: AttributeSet) : LinearLayout(context,
 class Mytext(context: Context, attrs: AttributeSet) :
     androidx.appcompat.widget.AppCompatTextView(context, attrs) {
     private var textmgListener: MyGestureListener? = null
-    private var textmDetector: GestureDetector? = null
+    private var textmDetector: GestureDetectorCompat
     init {
         textmgListener = MyGestureListener("這是Text發送")
-        textmDetector = GestureDetector(context,textmgListener)
+        textmDetector = GestureDetectorCompat(context, textmgListener)
     }
+
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-//        Timber.d("     demo,mytext------------onTouchEvent------------  ${event.toString()}")
-        textmDetector!!.onTouchEvent(event)
-        return true
-//        return super.onTouchEvent(event)
+        Timber.d("     demo,mytext------------onTouchEvent------------  ${event.toString()}")
+//        textmDetector!!.onTouchEvent(event)
+//        return true
+        return super.onTouchEvent(event)
     }
 
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
-//        Timber.d("     demo,mytext------------dispatchTouchEvent------  ${event.toString()}")
+        Timber.d("     demo,mytext------------dispatchTouchEvent------  ${event.toString()}")
         return super.dispatchTouchEvent(event)
     }
 
